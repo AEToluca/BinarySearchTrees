e2ce5785a20e8c6517f1c42c93e894637094c331
AEToluca <aeestrada1004@gmail.com>
2025-04-21 15:41:02 -0400

    Added comments and fixed some things in BST

--- Trees/BST.py
+++ Trees/BST.py
@@ -13,8 +13,8 @@ class BST:
     def __init__(self):
         self.root = None
         self.size = 0
-        self.height = 0
 
+    #Helper find method
     def find(self, time: int) -> Optional[Node]:
         current = self.root
         while current:
@@ -26,6 +26,7 @@ class BST:
                 current = current.right
         return None
 
+    #Insert method either replaces node.val == time or creates a new node
     def insert(self, time: int, request: Request):
         #In case of empty tree
         if not self.root:
@@ -56,15 +57,17 @@ class BST:
                 current.req = request
                 return
 
+    #Delete method recursively removes the node containing value time 
+    #Returns None of no such node
     def delete(self, time: int):
         def _delete(node: Optional[BST.Node], time: int) -> Optional[BST.Node]:
             #given node does not exist, return None
             if not node:
                 return None
             
-            if time < node.val:
+            if time < node.val:     #Traverse the left subtree if time less than val
                 node.left = _delete(node.left, time)
-            elif time > node.val:
+            elif time > node.val:   #Traverse the right subtree if the time is greater than val
                 node.right = _delete(node.right, time)
             else:
 
@@ -72,53 +75,55 @@ class BST:
                 if not node.left and not node.right:
                     return None
                 
-                #Case 2: Node has one child
+                #Case 2: Node has one child, return the existing child
                 if not node.left:
                     return node.right
                 if not node.right:
                     return node.left
                 
                 #Case 3: Node has two children
-                successor = self.succ(time)
-                node.val = successor.val
-                node.right = _delete(node.right, successor.val)
+                successor = self.succ(time)     
+                node.val = successor.val    #Swap values with successor using succ() helper
+                node.right = _delete(node.right, successor.val) #Remove node
 
             return node
         self.root = _delete(self.root, time)
+        self.size -= 1
 
-
+    #Helper method finds predeccessor of node with key of argument time
     def pred(self, time) -> Optional[Node]:
         current = self.root
         pred = None
         while current:
-            if time == current.val:
+            if time == current.val: #The val matches time
                 if current.left:
                     pred = current.left
-                    while pred.right:
+                    #Pred is the rightmost node of the nodes left subtree
+                    while pred.right:   
                         pred = pred.right
                     return pred
-            elif time < current.val:
+            elif time < current.val:    #Search for the node with the  time
                 current = current.left
             else:
                 pred = current
                 current = current.right
         return pred
                 
-        
+    #Helper method to find the successorof node with key of time
     def succ(self, time) -> Optional[Node]:
         current = self.root
         succ = None
         
         while current:
-            if time == current.val:
-                # If right subtree exists, successor is minimum in right subtree
+            if time == current.val: #The val matches time
                 if current.right:
                     succ = current.right
-                    while succ.left:
+                    #Succ is the leftmost node of the nodes right subtree
+                    while succ.left:    
                         succ = succ.left
                 return succ
-            elif time < current.val:
-                # Keep track of the smallest value greater than time
+            elif time < current.val:    #Search for the node with time
+                
                 succ = current
                 current = current.left
             else:
@@ -126,20 +131,25 @@ class BST:
         
         return succ
 
+    #Method that finds the smallest value of the tree
     def min(self) -> Optional[Node]:
+        #Min of a bst is the leftmost node
         current = self.root
         while current.left:
             current = current.left
 
         return current
 
+    #Method that finds the largest value of the tree
     def max(self) -> Optional[Node]:
+        #Max of a bst is the rightmost node
         current = self.root
         while current.right:
             current = current.right
 
         return current
 
+    #Print the contents of the tree in sorted order
     def print(self):
         stack = []
         current = self.root
@@ -153,6 +163,7 @@ class BST:
             print(current.val, end= ' ')
             current = current.right
 
+#Driver main method used to test BST methods
 def main():
     bst = BST()
     bst.insert(20, Request.Request("r", 10, "Airline1", "A1", "CityA", "CityB"))
