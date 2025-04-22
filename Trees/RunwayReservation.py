05179ccb7680baef31746f314df2b90065caea75
AEToluca <aeestrada1004@gmail.com>
2025-04-21 22:13:27 -0400

    Completed and tested runway reservation

--- Trees/RunwayReservation.py
+++ Trees/RunwayReservation.py
@@ -30,24 +30,38 @@ class RunwayReservation:
             except EOFError:
                 break
         self.process()
-
-    def process(self):
+    #Handles all requests according to their command
+    def process(self): 
         for request in self.requests:
             if request.command == 'r':
                 self.handle_r(request)
             if request.command == 't':
                 self.handle_t(request.time)
+        
+        #Handles any remaining flights at the end
+        if self.runway.size > 0:
+            last_time = self.runway.max().val
+            time_to_add = last_time - self.current_time
+            if time_to_add > 0:
+                self.handle_t(time_to_add)
 
+    #Handles the case where the command is a request to reserve a runway
     def handle_r(self, request: Request):
+        #If the request time has already passed, do nothing
+        if request.time < self.current_time:
+            return
+            
+        #Check if the request time is within the grace period of the predecessor and successor
         pred = self.runway.pred(request.time)
         succ = self.runway.succ(request.time)
         if (pred is None or request.time - pred.val >= self.k) and (succ is None or succ.val - request.time >= self.k):
             self.runway.insert(request.time, request)
 
-    def handle_t(self, new_time: int):
-        print(f"Current time = {new_time}")
-        self.current_time += new_time
-
+    #Handles the case where the command is a request to advance the time
+    def handle_t(self, time_increment: int):
+        self.current_time += time_increment
+        print(f"Current time = {self.current_time} units")
+        #Print and remove all flights whose time has passed
         while self.runway.size > 0:
             min_node = self.runway.min()
             if min_node.val > self.current_time:
